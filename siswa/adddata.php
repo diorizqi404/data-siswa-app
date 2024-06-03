0ca8cc4ada9acee80c67ee388a1adff167bb0d87
Glaezz <72900598+Glaezz@users.noreply.github.com>
2024-06-03 12:26:05 +0700

    Update adddata.php

--- siswa/adddata.php
+++ siswa/adddata.php
@@ -2,7 +2,7 @@
 include "conn.php";
 $nis	= @$_REQUEST['nis'];
 $nama	= @$_REQUEST['nama'];
-$jk		= @$_REQUEST['jk'];
+$jk  = @$_REQUEST['jk'];
 $alamat	= @$_REQUEST['alamat'];
 $nomor	= @$_REQUEST['nomor'];
 $connect->query("INSERT INTO siswa (nis, nama, jk, alamat, nomor) VALUES ('$nis', '$nama', '$jk', '$alamat', '$nomor')");
